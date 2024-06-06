a66c8c7b4873b3c7aa51e0f2dc9de07f67c02b25
ssushilgit <shresthasushil741@gmail.com>
2024-06-06 12:30:33 +0545

    Updated Login Signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,37 +1,52 @@
-import React from 'react'
-import email_icon from '../../components/assets/email.png'
-import password_icon from '../../components/assets/password.png'
-import { Link } from 'react-router-dom'
-import './Login.css'
+import React from 'react';
+import user_icon from '../../components/assets/person.png';
+import email_icon from '../../components/assets/email.png';
+import password_icon from '../../components/assets/password.png';
+import login_image from '../../components/assets/login.svg'; // Add your image path here
+import { Link } from 'react-router-dom';
+import './Login.css';
 
-const Login = () => {
+const Signin = () => {
     return (
-        <div className='signin'>
-            <div className="header">
-                <div className="text">Login</div>
-                <div className="underline"></div>
+        <div className='signin-container'>
+            <div className='header-title'>
+                Calendar Management System
             </div>
-            <div className="inputs">
-                <div className="input">
-                    <img src={email_icon} alt="Email Icon" />
-                    <input type="email" placeholder='Email ID' />
+            <div className='main-content'>
+                <div className='image-section'>
+                    <img src={login_image} alt="Signup" />
                 </div>
-                <div className="input">
-                    <img src={password_icon} alt="Password Icon" />
-                    <input type="password" placeholder='Password' />
+                <div className='signin'>
+                    <div className="header">
+                        <div className="text">Login</div>
+                        <div className="underline"></div>
+                    </div>
+                    <div className="inputs">
+
+                        <div className="input">
+                            <img src={email_icon} alt="" />
+                            <input type="email" placeholder='Email ID' />
+                        </div>
+                        <div className="input">
+                            <img src={password_icon} alt="" />
+                            <input type="password" placeholder='Password' />
+                        </div>
+                    </div>
+
+                    <div className="submit-container">
+                        <div className="submit">Login</div>
+                    </div>
+
+                    <div className="forgot-password">
+                        Don't have an account?
+                        <span>
+                            <Link to='/signup'> Click Here!</Link>
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <div className="submit-container">
-                <div className="submit">Submit</div>
-            </div>
-            <div className="forgot-password">
-                Don't have an account?
-                <span>
-                    <Link to='/signup'> Click Here!</Link>
-                </span>
             </div>
         </div>
-    )
+    );
 }
 
-export default Login
+export default Signin;
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,44 +1,55 @@
-import React from 'react'
-import user_icon from '../../components/assets/person.png'
-import email_icon from '../../components/assets/email.png'
-import password_icon from '../../components/assets/password.png'
-import './Signin.css'
+import React from 'react';
+import user_icon from '../../components/assets/person.png';
+import email_icon from '../../components/assets/email.png';
+import password_icon from '../../components/assets/password.png';
+import login_image from '../../components/assets/login.svg'; // Add your image path here
+import { Link } from 'react-router-dom';
+import './Signin.css';
+
 const Signin = () => {
-
-
     return (
-        <div className='signin'>
-            <div className="header">
-                <div className="text">Sign Up</div>
-                <div className="underline"></div>
+        <div className='signin-container'>
+            <div className='header-title'>
+                Calendar Management System
             </div>
-            <div className="inputs">
-                <div className="input">
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder='Name' />
+            <div className='main-content'>
+                <div className='image-section'>
+                    <img src={login_image} alt="Signup" />
                 </div>
-                <div className="input">
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder='Email ID' />
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder='Password' />
-                </div>
-            </div>
+                <div className='signin'>
+                    <div className="header">
+                        <div className="text">Sign Up</div>
+                        <div className="underline"></div>
+                    </div>
+                    <div className="inputs">
+                        <div className="input">
+                            <img src={user_icon} alt="" />
+                            <input type="text" placeholder='Name' />
+                        </div>
+                        <div className="input">
+                            <img src={email_icon} alt="" />
+                            <input type="email" placeholder='Email ID' />
+                        </div>
+                        <div className="input">
+                            <img src={password_icon} alt="" />
+                            <input type="password" placeholder='Password' />
+                        </div>
+                    </div>
 
-            <div className="submit-container">
-                <div className="submit">Submit</div>
-            </div>
+                    <div className="submit-container">
+                        <div className="submit">Submit</div>
+                    </div>
 
-            <div className="forgot-password">
-                Already have an account? 
-                <span>
-                    Login
-                </span>
+                    <div className="forgot-password">
+                        Already have an account? 
+                        <span>
+                           <Link to='/login'> Login </Link>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Signin
+export default Signin;
